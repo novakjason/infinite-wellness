2573b5ed41487c12ff7ce31311ddb6506b1c639f
Jason Novak <novak.jason7@gmail.com>
2020-02-20 03:43:10 -0500

    Format and adjust comments

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,16 +1,19 @@
 const router = require('express').Router();
 const userController = require('../../controllers/userController');
 
-// Matches with "/api/users"
+// matches with "/api/users"
 router.route('/')
-  .get(userController.findAll)
-  .post(userController.create);
+    .get(userController.findAll);
 
-// Matches with "/api/users/:id"
+// matches with "/api/users/:id"
 router
-  .route('/:id')
-  .get(userController.findById)
-  .put(userController.update)
-  .delete(userController.remove);
+    .route('/:id')
+    .get(userController.findById)
+    .put(userController.update)
+    .delete(userController.remove);
+
+// matches with "/api/users/register"
+router.route("/register")
+    .post(userController.create);
 
 module.exports = router;
