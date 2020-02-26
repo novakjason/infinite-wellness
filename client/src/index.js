2b3e66b04875c0673c18d64e05b0e397b8835069
Jason Novak <novak.jason7@gmail.com>
2020-02-26 13:24:40 -0500

    updated serviceWorker

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,8 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import App from "./App";
+import * as serviceWorker from './serviceWorker';
 
-import registerServiceWorker from './registerServiceWorker';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render( <App /> , document.getElementById('root'));
-
-registerServiceWorker();
+serviceWorker.unregister();
